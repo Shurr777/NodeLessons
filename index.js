6a7b3999973942bdc094e6e56a51ea53779412a7
Alex <Almarkiz777@gmail.com>
2024-08-02 17:41:55 +0300

    mongoose database connected

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const Application = require('./myframework/Application');
 const userRouter = require('./src/user-router');
 const jsonParser = require('./myframework/parseJson')
 const parseUrl = require('./myframework/parseUrl');
+const mongoose = require('mongoose');
 const app = new Application();
+const uri = "mongodb+srv://almarkiz777:gh0CE4jCTFUtrXN5@cluster123.qrlzkxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster123";
 
 app.use(jsonParser);
 app.use(parseUrl('http://localhost:' + PORT));
 app.addRouter(userRouter)
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+//gh0CE4jCTFUtrXN5
+
+const start = async () => {
+    try {
+        await mongoose.connect(uri)
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
--- src/user-controller.js
+++ src/user-controller.js
@@ -1,23 +1,37 @@
-const users = [
+/*const users = [
     {id: 1, name: 'John', email: 'john@example.com'},
     {id: 2, name: 'Jane', email: 'jane@example.com'},
     {id: 3, name: 'Petya', email: 'petya@example.com'},
     {id: 4, name: 'Vasya', email: 'vasya@example.com'},
-]
+]*/
 
-const getUsers = (req, res) => {
-    console.log(req.params)
+const User = require('./user-model');
+
+const getUsers = async (req, res) => {
+    // Работа с массивом выше
+    /*console.log(req.params)
     if (req.params.id) {
         return res.send(users.find(user => user.id.toString() === req.params.id))
+    }*/
+    //
+    let users;
+    if (req.params.id) {
+        users = await User.findById(req.params.id)
+    } else {
+        users = await User.find()
     }
+
     res.send(users);
 }
 
-const createUser = (req, res) => {
-    console.log(req.body);
+const createUser = async (req, res) => {
+    // Работа с массивом выше
+    /*console.log(req.body);
     const user = req.body;
-    users.push(user);
-    res.send(users);
+    users.push(user);*/
+    //
+    const user = await User.create(req.body)
+    res.send(user);
 }
 
 module.exports = {
